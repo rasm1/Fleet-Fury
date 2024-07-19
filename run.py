51bb0cadd6bf97e7777666067d5707fa618c3fdf
Mees R. <rasmees123@gmail.com>
2024-07-19 14:08:39 +0000

    Add function for getting number of ships to play with

--- run.py
+++ run.py
@@ -8,6 +8,18 @@ class Board:
         self.player_name = player_name
         self.type = type
 
+    def print_board(self):
+        print("_"* 6)
+
+
+def get_num_of_ships():
+    """
+    Get number of ships to populate the board with
+    """
+    
+    num_of_ships = input("Please enter the amount of ships you want to play with:")
+    print(f"You have entered: {num_of_ships} ships")
+
 def playGame():
     print("_"*30)
     print("Welcome to Fleet fury")
@@ -15,7 +27,11 @@ def playGame():
     print("press a key")
     print("_"*30)
     player_name = input("please enter your name here:")
+    print(f"Your name is: {player_name}\n")
+
+    player_board = Board(6,4,player_name,type = "player")
 
 
 
 playGame()
+get_num_of_ships()
