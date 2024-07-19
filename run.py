05b4aa62d31339f415349e6e3678f8bedaa530bd
Mees R. <rasmees123@gmail.com>
2024-07-19 13:38:51 +0000

    Add main board class

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
+class Board:
+    """
+    Main class for the game board.
+    """
+    def __init__(self,board_size,num_of_ships,player_name,type):
+        self.board_size = board_size
+        self.num_of_ships = num_of_ships
+        self.player_name = player_name
+        self.type = type
 
 def playGame():
     print("_"*30)
@@ -5,5 +14,8 @@ def playGame():
     print("A console based battleships game")
     print("press a key")
     print("_"*30)
+    player_name = input("please enter your name here:")
+
+
 
 playGame()
