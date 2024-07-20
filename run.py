e5b9a577ec9f2a38a4f43836c0fbfa4071352474
Mees R. <rasmees123@gmail.com>
2024-07-20 14:29:08 +0000

    Add playing grid

--- run.py
+++ run.py
@@ -3,13 +3,11 @@ class Board:
     Main class for the game board.
     """
     def __init__(self,board_size,num_of_ships,player_name,type):
-        self.board_size = board_size
+        self.board_size = [["_" for x in range(board_size)] for y in range(board_size)]
         self.num_of_ships = num_of_ships
         self.player_name = player_name
         self.type = type
 
-    def print_board(self):
-        print("_"* 6)
 
 
 def get_num_of_ships():
@@ -36,14 +34,22 @@ def validate_num_of_ships():
     except ValueError as ve:
         print(f"invalid input: {ve}")
 
-    
+def print_board():
+    """
+    prints the play grid
+    """
+    print(f"{player_name}'s board:")
+    print("  0  "," 1 ","  2  "," 3  "," 4  "," 5  ")
+    for _ in range(6):
+        print(_, "  _  "*6)
 
-def playGame():
+
+def RunGame():
     print("_"*30)
     print("Welcome to Fleet fury")
     print("A console based battleships game")
-    print("press a key")
     print("_"*30)
+    global player_name
     player_name = input("please enter your name here:")
     
 
@@ -51,7 +57,8 @@ def playGame():
 
 
 def main():
-    playGame()
+    RunGame()
     get_num_of_ships()
+    print_board()
 
 main()
